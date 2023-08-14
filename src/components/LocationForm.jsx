bcaea389fe40856239dd121c2bd56a40b17c01a9
juangruoficial <cruz9606@gmail.com>
2023-08-14 15:25:25 -0500

    adding animation to welcome letter

--- src/components/LocationForm.jsx
+++ src/components/LocationForm.jsx
@@ -1,16 +1,33 @@
+import { useEffect, useState } from "react";
 import "./styles/LocationForm.css";
 const LocationForm = ({ LIMIT, handleSubmit }) => {
+  const [animate, setAnimate] = useState(false);
+  const welcomeMessage = "Welcome to rick Universe lets have some fun";
+
+  useEffect(() => {
+    setAnimate(true);
+  }, []);
+
   return (
     <header className="wrapper-form">
       <h3 className="welcome-text">
-        Welcome to rick Universe lets have some fun
+        {welcomeMessage.split("").map((char, charIndex) => (
+          <span
+            key={charIndex}
+            className={`letter ${animate ? "appear" : ""}`}
+            style={{ animationDelay: `${charIndex * 80}ms` }}
+          >
+            {char === " " ? "\u00A0" : char}
+          </span>
+        ))}
       </h3>
+
       <form className="form" onSubmit={handleSubmit}>
         <input
           min={1}
           max={LIMIT}
           type="number"
-          placeholder="type a location id ....."
+          placeholder="type an id location (1 - 126)..."
           id="newLocation"
           className="form-input"
         ></input>

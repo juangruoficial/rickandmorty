2d31ffa4801e9f56ee1d41f642e7208cd3a089e1
juangruoficial <cruz9606@gmail.com>
2023-08-14 16:12:18 -0500

    background img

--- src/App.jsx
+++ src/App.jsx
@@ -46,7 +46,9 @@ function App() {
       <div className="vortice">
         <img className="vortice-img" src="/images/vortice.png" alt="" />
         <img className="name-img" src="/images/name.png" alt="name" />
+        <img className="ellipse-img" src="/images/ellipse.png" alt="" />
       </div>
+
       {currentLocation && (
         <>
           <LocationForm LIMIT={LIMIT} handleSubmit={handleSubmit} />
